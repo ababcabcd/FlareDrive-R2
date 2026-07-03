c410b59030009dd525c6518222a978b82e4327dc
feng <none>
2026-07-03 16:53:38 +0800

    update

--- assets/App.vue
+++ assets/App.vue
@@ -48,10 +48,20 @@
     <div class="file-list-container">
       <div class="toolbar">
         <div class="toolbar-path">
-          <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-            <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
-          </svg>
-          <span class="path-text" v-text="(cwd && cwd.charAt(0) === '/' ? cwd : '/' + (cwd || ''))"></span>
+          <button class="path-link" @click="cwd = ''">
+            <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+              <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
+            </svg>
+          </button>
+          <template v-if="cwd">
+            <span class="path-separator">/</span>
+            <template v-for="(segment, index) in cwdSegments" :key="index">
+              <button class="path-link" @click="navigateToPath(segment.path)">
+                {{ segment.name }}
+              </button>
+              <span class="path-separator" v-if="index < cwdSegments.length - 1">/</span>
+            </template>
+          </template>
         </div>
         <div class="toolbar-actions">
           <button class="toolbar-btn" @click="createFolder()">新建文件夹</button>
@@ -335,9 +345,23 @@ export default {
       }
       return folders;
     },
+
+    cwdSegments() {
+      if (!this.cwd) return [];
+      const path = this.cwd.replace(/\/$/, '');
+      const parts = path.split('/').filter(p => p);
+      return parts.map((part, index) => ({
+        name: part,
+        path: parts.slice(0, index + 1).join('/') + '/'
+      }));
+    },
   },
 
   methods: {
+    navigateToPath(path) {
+      this.cwd = path;
+    },
+
     copyLink(link) {
       const url = new URL(link, window.location.origin);
       navigator.clipboard.writeText(url.toString());
@@ -1000,6 +1024,27 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   color: #666;
+  font-size: 14px;
+}
+
+.path-link {
+  background: none;
+  border: none;
+  color: #4a90d9;
+  font-size: 14px;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  text-decoration: underline;
+}
+
+.path-link:hover {
+  color: #357abd;
+}
+
+.path-separator {
+  color: #999;
+  margin: 0 2px;
   font-size: 14px;
 }
 
