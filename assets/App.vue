c28efb9b0c14860828cdce7de9d8da3fcb4213ca
feng <none>
2026-07-03 17:22:10 +0800

    update

--- assets/App.vue
+++ assets/App.vue
@@ -47,12 +47,17 @@
     </div>
     <div class="file-list-container">
       <div class="toolbar">
-        <div class="toolbar-path">
+        <div ref="toolbarPathRef" class="toolbar-path">
           <button class="path-link" @click="cwd = ''">
             <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
               <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
             </svg>
           </button>
+          <span ref="pathWidthRef" class="path-width-measure" v-if="cwd">
+            <template v-for="(segment, index) in cwdSegments" :key="index">
+              <span>/</span>{{ segment.name }}
+            </template>
+          </span>
           <template v-if="cwd">
             <span class="path-separator">/</span>
             <template v-for="(segment, index) in visiblePathSegments" :key="index">
@@ -332,8 +337,36 @@ export default {
     shareError: null,
     showShareManagement: false,
     shares: [],
-    sharesLoading: false
+    sharesLoading: false,
+    pathWidth: 0,
+    containerWidth: 0
   }),
+
+  mounted() {
+    this.updatePathWidth();
+    window.addEventListener('resize', this.updatePathWidth);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('resize', this.updatePathWidth);
+  },
+
+  watch: {
+    cwd() {
+      this.$nextTick(() => this.updatePathWidth());
+    }
+  },
+
+  methods: {
+    updatePathWidth() {
+      const pathEl = this.$refs.pathWidthRef;
+      const containerEl = this.$refs.toolbarPathRef;
+      if (pathEl && containerEl) {
+        this.pathWidth = pathEl.offsetWidth;
+        this.containerWidth = containerEl.offsetWidth;
+      }
+    },
+  },
 
   computed: {
     filteredFiles() {
@@ -354,12 +387,24 @@ export default {
       return folders;
     },
 
+    cwdSegments() {
+      if (!this.cwd) return [];
+      const path = this.cwd.replace(/\/$/, '');
+      const parts = path.split('/').filter(p => p);
+      return parts.map((part, index) => ({
+        name: part,
+        path: parts.slice(0, index + 1).join('/') + '/'
+      }));
+    },
+
     visiblePathSegments() {
       if (!this.cwd) return [];
       const path = this.cwd.replace(/\/$/, '');
       const parts = path.split('/').filter(p => p);
       
-      if (parts.length <= 2) {
+      const needsEllipsis = this.pathWidth > this.containerWidth;
+      
+      if (!needsEllipsis || parts.length <= 2) {
         return parts.map((part, index) => ({
           name: part,
           path: parts.slice(0, index + 1).join('/') + '/',
@@ -1050,6 +1095,7 @@ export default {
   flex: 1;
   overflow: hidden;
   min-width: 0;
+  position: relative;
 }
 
 .toolbar-path .path-text {
@@ -1059,6 +1105,15 @@ export default {
   text-overflow: ellipsis;
   color: #666;
   font-size: 14px;
+}
+
+.path-width-measure {
+  position: absolute;
+  visibility: hidden;
+  white-space: nowrap;
+  font-size: 14px;
+  height: 0;
+  overflow: hidden;
 }
 
 .path-link {
